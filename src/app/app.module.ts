be8baf8d96e80ace5741be3f47fb1a5de4675a00
AliArmas <191287@ids.upchiapas.edu.mx>
2020-09-09 13:55:05 -0500

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NvarComponent } from './components/nvar/nvar.component';
+import { FooterAppComponent } from './components/footer-app/footer-app.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NvarComponent,
+    FooterAppComponent,
+    ProductosComponent,
+    LogInComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
